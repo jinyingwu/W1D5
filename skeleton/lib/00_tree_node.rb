69eb65ad6086b98d9b676561e318f429cbc15ec0
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-24 12:15:50 -0700

    add #parent= and inti to node

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,24 @@
 class PolyTreeNode
-
+   
+  def initialize(value)
+    @value = value
+    @parent = nil 
+    @children = []
+  end
+  
+  attr_reader :value, :parent
+  attr_accessor :children
+  
+  def parent=(my_parent)
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = my_parent
+    
+    unless @parent.nil?
+      my_parent.children << self unless my_parent.children.include?(self)
+    end 
+  end 
+  
+  def add_child
+  end 
+  
 end
